6f41aebbf0420e1f4941f1e6ac1e867a40102069
이학빈 <leehakbin1217@gmail.com>
2026-02-10 10:44:01 +0900

    feat: 회원가입 페이지 구현 및 하드코딩으로 API 연동

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+// 1. 파일명 대소문자를 꼭 확인하세요! (실제 파일이 SignUpPage.jsx라면 아래처럼)
+import SignUpPage from './pages/SignUpPage'; 
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          {/* 2. 주소창에 아무것도 안 쳤을 때(/) 바로 회원가입 페이지를 보여줍니다 */}
+          <Route path="/" element={<SignUpPage />} />
+          
+          {/* 3. 혹시 모르니 /signup 경로도 연결해둡니다 */}
+          <Route path="/signup" element={<SignUpPage />} />
+
+          {/* 4. 없는 페이지로 들어오면 홈(/)으로 보내버립니다 */}
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
